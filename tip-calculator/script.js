33fbf16bc976540592ea946935a846753d2e36fb
acemaster-gh <acewithmefrom0@gmail.com>
2026-02-26 20:05:05 +0530

    code structure issue resolved

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -2,23 +2,24 @@ const bill = document.querySelector('#bill');
 const tip = document.querySelector('#tip');
 const calculateBtn = document.querySelector('#calculateBtn');
 const totalAmount = document.querySelector('#total');
-const container = document.querySelector('.container');
 
 
 calculateBtn.addEventListener('click', function() {
     const billValue = parseFloat(bill.value);
     const tipValue = parseFloat(tip.value);
-    const tipAmount = billValue * tipValue
-    const total = billValue + tipAmount;
 
-
-    if (isNaN(billValue) || billValue <= 0) {
+        if (isNaN(billValue) || billValue <= 0) {
         totalAmount.textContent = "Please enter a valid bill amount";
         totalAmount.style.color = "red";
         return;
 
-        
     }
+
+
+    const tipAmount = billValue * tipValue;
+    const total = billValue + tipAmount;
+
+
 
 
    totalAmount.style.color = "white"; 
