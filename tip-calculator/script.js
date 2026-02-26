172605bb9fc9bdedc2ecb6680a64fd10e661813a
acemaster-gh <acewithmefrom0@gmail.com>
2026-02-26 19:57:10 +0530

    Initial commit - tip calculator created

---
+++ tip-calculator/script.js
@@ -0,0 +1,30 @@
+const bill = document.querySelector('#bill');
+const tip = document.querySelector('#tip');
+const calculateBtn = document.querySelector('#calculateBtn');
+const totalAmount = document.querySelector('#total');
+const container = document.querySelector('.container');
+
+
+calculateBtn.addEventListener('click', function() {
+    const billValue = parseFloat(bill.value);
+    const tipValue = parseFloat(tip.value);
+    const tipAmount = billValue * tipValue
+    const total = billValue + tipAmount;
+
+
+    if (isNaN(billValue) || billValue <= 0) {
+        totalAmount.textContent = "Please enter a valid bill amount";
+        totalAmount.style.color = "red";
+        return;
+
+        
+    }
+
+
+   totalAmount.style.color = "white"; 
+    totalAmount.textContent = `Total: ₹${total.toFixed(2)}`;
+    
+totalAmount.textContent = `Total: ${total.toFixed(2)}`;
+
+
+});
